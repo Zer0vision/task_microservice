010717dd3c3ef7c8f6f3134452fe197de4caf94e
X <aps28@tpu.ru>
2025-04-16 17:17:51 +0000

    Fixed DATABASE_URL to use env variable

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://user:password@db/tasks"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql://user:password@mysql:3306/tasks")
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
